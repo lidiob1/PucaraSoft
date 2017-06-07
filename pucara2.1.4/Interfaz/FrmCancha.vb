f8b5a8978d54cf90592cfaa901d44ae4e3c86ded
lidiob1 <lidiob1@gmail.com>
2017-06-06 23:16:56 -0300

    Revert "Revert "Modificacion disponibilidad y DB""
    This reverts commit a6a25bbf99176706995cc3af181f9bd2516de2bf.

--- pucara2.1.4/Interfaz/FrmCancha.vb
+++ pucara2.1.4/Interfaz/FrmCancha.vb
@@ -371,9 +371,9 @@ Public Class FrmCancha
         BtnGuardar_Cancha.Visible = False
     End Sub
     '*************************************************HACER INVISIBLE ROWS****************************
-    Public Function OcultarFilas()
+    Public Sub OcultarFilas()
         DataListado_Cancha.Columns(4).Visible = False
-    End Function
+    End Sub
     Private Sub CbInhabilitar_Cancha_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CbInhabilitar_Cancha.CheckedChanged
         If CbInhabilitar_Cancha.CheckState = CheckState.Checked Then
             DataListado_Cancha.Columns.Item("Inhabilitar").Visible = True
--- pucara2.1.4/Interfaz/FrmDisponibilidad.vb
+++ pucara2.1.4/Interfaz/FrmDisponibilidad.vb
@@ -10,6 +10,8 @@ Public Class FrmDisponibilidad
         'mostrar_horario()
         CargarTipoCancha()
 
+        cargar_grilla()
+
     End Sub
 
     Public Sub mostrar_horario()
@@ -18,9 +20,22 @@ Public Class FrmDisponibilidad
         dgv_canchas.DataSource = Nothing
         dgv_canchas.DataSource = dt
     End Sub
-    '--------------------------------------------------------------------------------
+
+    '=======================================================================================================
+    ' Cargar el combobox de Tipo de Cancha
+    '=======================================================================================================
+    Private Sub CargarTipoCancha()
+        Dim objComboTipoCancha As New CanchaDA
+        ds = New DataSet()
+        ds = objComboTipoCancha.cargarTipoCancha
+        cb_tipo_cancha.DataSource = ds.Tables("tabTipoCancha")
+        cb_tipo_cancha.DisplayMember = "descripcion"
+        cb_tipo_cancha.ValueMember = "id_tipo_cancha"
+    End Sub
+
+    '=======================================================================================================
     ' Completamos combo canchas
-    '--------------------------------------------------------------------------------
+    '=======================================================================================================
     Public Sub completar_cb_cancha()
         Dim objComboCanchas As New CanchaDA
         Dim objTipoCancha As New TipoCanchaNE
@@ -34,17 +49,7 @@ Public Class FrmDisponibilidad
         cb_nro_cancha.ValueMember = "Num_cancha"
     End Sub
 
-    '=======================================================================================================
-    ' Cargar el combobox de Tipo de Cancha
-    '=======================================================================================================
-    Private Sub CargarTipoCancha()
-        Dim objComboTipoCancha As New ReservaDA
-        ds = New DataSet()
-        ds = objComboTipoCancha.cargarTipoCancha
-        cb_tipo_cancha.DataSource = ds.Tables("tabTipoCancha")
-        cb_tipo_cancha.DisplayMember = "descripcion"
-        cb_tipo_cancha.ValueMember = "id_tipo_cancha"
-    End Sub
+
 
     '=======================================================================================================
     ' Cuando elijo del combobox tipo de cancha
@@ -52,4 +57,58 @@ Public Class FrmDisponibilidad
     Private Sub cb_tipo_cancha_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cb_tipo_cancha.SelectedIndexChanged
         completar_cb_cancha()
     End Sub
+
+    '=======================================================================================================
+    ' Cargar grilla
+    '=======================================================================================================
+    Private Sub cargar_grilla()
+
+        With dgv_canchas
+            '.Columns(0).Name = "colHoraInicio"
+            'Agregamos una fila con los siguientes datos
+            .Rows.Add("08:00", "09:00", "5 vs 5")
+            'Alineamos a la derecha
+            .Columns(0).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight
+            .Columns(1).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight
+            .Columns(2).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight
+        End With
+
+
+
+        ''elimino primera columna de selección de la grilla
+        'dgv_canchas.RowHeadersVisible = False
+
+        ''Creo mi datatable y columnas
+        'Dim miDataTable As New DataTable
+        'miDataTable.Columns.Add("Nombre")
+        'miDataTable.Columns.Add("Sexo")
+
+        ''Renglon es la variable que adicionara renglones a mi datatable
+        'Dim Renglon As DataRow = miDataTable.NewRow()
+        'Renglon("Nombre") = "Luis"
+        'Renglon("Sexo") = "Masculino"
+        'miDataTable.Rows.Add(Renglon)
+
+        'Renglon = miDataTable.NewRow()
+        'Renglon("Nombre") = "Carmen"
+        'Renglon("Sexo") = "Femenino"
+        'miDataTable.Rows.Add(Renglon)
+
+        ''por último envió mi datatable a un gridview para visualizarlo en pantalla
+        'Me.dgv_canchas.DataSource = miDataTable
+        ''Me.dgv_canchas.DataBind()
+    End Sub
+
+    '=======================================================================================================
+    ' Alineamos los campos a la derecha en la grilla
+    '=======================================================================================================
+    Private Sub alinear_derecha()
+        With dgv_canchas
+            'Alineamos a la derecha
+            .Columns(0).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight
+            .Columns(1).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight
+            .Columns(2).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight
+        End With
+    End Sub
+
 End Class
